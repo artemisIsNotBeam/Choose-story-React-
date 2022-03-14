9b4aaed4fb9f9ffa9c0b86703fb05b05be3f4fce
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-14 07:16:58 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>This is my first react app</p>
+    </div>
+  );
+}
+
+export default App;
